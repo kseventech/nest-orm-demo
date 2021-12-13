31bb830d4659a7b262afc5f9b32ad0171ce7c8e3
Milos Vuksanovic <hex3solutions@gmail.com>
2021-12-13 15:11:06 +0100

    Add user and cat entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeormConfig } from './config/typeorm.config';
+import { CatModule } from './entities/cat/cat.module';
+import { UserModule } from './entities/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRootAsync({ useClass: TypeormConfig })],
+  imports: [TypeOrmModule.forRootAsync({ useClass: TypeormConfig }), CatModule, UserModule],
   controllers: [AppController],
   providers: [],
 })
