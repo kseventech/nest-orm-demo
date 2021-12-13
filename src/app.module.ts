8351e5819e05193d12065822795c18e03165b751
Milos Vuksanovic <hex3solutions@gmail.com>
2021-12-13 14:57:31 +0100

    Add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeormConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRootAsync({ useClass: TypeormConfig })],
   controllers: [AppController],
   providers: [],
 })
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { getMetadataArgsStorage } from 'typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+@Injectable()
+export class TypeormConfig {
+  async createTypeOrmOptions() {
+    const options: PostgresConnectionOptions = {
+      type: 'postgres',
+      username: 'user_1',
+      password: 'test1',
+      entities: getMetadataArgsStorage().tables.map((tbl) => tbl.target),
+      synchronize: false,
+      migrations: ['dist/src/db/migrations/*.js'],
+      cli: {
+        migrationsDir: 'src/db/migrations',
+      },
+    };
+    return options;
+  }
+}
