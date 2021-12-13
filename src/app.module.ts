216374de787a7ae96fc08f68767897f2c346c489
Milos Vuksanovic <hex3solutions@gmail.com>
2021-12-13 13:36:05 +0100

    Connect Sequelize Module to Postgress

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { SequelizeConfig } from './config/sequelize.config';
 
 @Module({
-  imports: [],
+  imports: [SequelizeModule.forRootAsync({ useClass: SequelizeConfig })],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/config/sequelize.config.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { SequelizeOptionsFactory, SequelizeModuleOptions } from '@nestjs/sequelize';
+
+@Injectable()
+export class SequelizeConfig implements SequelizeOptionsFactory {
+  createSequelizeOptions(): SequelizeModuleOptions {
+    return {
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user_1',
+      password: 'test1',
+      database: 'orm-demo-db',
+      autoLoadModels: true,
+      synchronize: true,
+    };
+  }
+}
