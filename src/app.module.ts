da13d1f04915df0306e991ab2f74661baf4d4f99
Milos Vuksanovic <hex3solutions@gmail.com>
2021-12-15 16:21:22 +0100

    Change config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CatModule } from './entities/cat/cat.module';
 import { UserModule } from './entities/user/user.module';
-import ormConfig from './config/postgres.config';
-// import { ormConfig }  from 'ormconfig';
+import typeormConfig from './config/typeorm.config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormConfig), CatModule, UserModule],
+  imports: [TypeOrmModule.forRoot(typeormConfig), CatModule, UserModule],
   controllers: [AppController],
   providers: [],
 })
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,23 +1,16 @@
-import { Injectable } from '@nestjs/common';
-import { Cat } from 'src/entities/cat/models/cat.entity';
-import { User } from 'src/entities/user/models/user.entity';
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-@Injectable()
-export class TypeormConfig {
-  async createTypeOrmOptions() {
-    const options: PostgresConnectionOptions = {
-      type: 'postgres',
-      username: 'user_1',
-      password: 'test1',
-      database: 'typeorm-demo-db',
-      entities: [User, Cat],
-      synchronize: false,
-      migrations: ['dist/src/db/migrations/*.js'],
-      cli: {
-        migrationsDir: 'src/db/migrations',
-      },
-    };
-    return options;
-  }
-}
+const typeormConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  // driver: 'postgres',
+  username: 'user_1',
+  password: 'test1',
+  database: 'typeorm-demo-db',
+  entities: ['dist/src/**/*.entity.js'],
+  synchronize: false,
+  migrations: ['dist/src/db/migrations/*.js'],
+  cli: {
+    migrationsDir: 'src/db/migrations',
+  },
+};
+export default typeormConfig;
